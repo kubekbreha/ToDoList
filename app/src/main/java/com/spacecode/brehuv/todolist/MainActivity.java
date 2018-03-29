ad2015fa785398bb5bf15fb5450ac16ef8a025ce
kubekbreha <kubek.breha@gmail.com>
2018-03-29 15:43:05 +0200

    login working core

--- app/src/main/java/com/spacecode/brehuv/todolist/MainActivity.java
+++ app/src/main/java/com/spacecode/brehuv/todolist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.spacecode.brehuv.todolist;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/spacecode/brehuv/todolist/login/fragment/LoginEmailFragment.java
@@ -0,0 +1,174 @@
+/*
+* Copyright 2018 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.spacecode.brehuv.todolist.login.fragment;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.spacecode.brehuv.todolist.MainActivity;
+import com.spacecode.brehuv.todolist.R;
+
+
+/**
+ * Created by kubek on 1/21/18.
+ */
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginEmailFragment extends Fragment {
+
+    private EditText mLoginEmailField;
+    private EditText mLoginPasswordField;
+    private Button mLoginButton;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    private ProgressDialog mProgres;
+    private AnimationDrawable mAnimationDrawable;
+    private RelativeLayout mRelativeLayout;
+
+    public LoginEmailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment\
+        View view = inflater.inflate(R.layout.fragment_login_email, container, false);
+
+        mRelativeLayout = view.findViewById(R.id.email_login_gradient);
+
+        mLoginButton = view.findViewById(R.id.login_email_button);
+        mLoginEmailField = view.findViewById(R.id.login_email);
+        mLoginPasswordField = view.findViewById(R.id.login_password);
+
+        mAnimationDrawable = (AnimationDrawable) mRelativeLayout.getBackground();
+        mAnimationDrawable.setEnterFadeDuration(4500);
+        mAnimationDrawable.setExitFadeDuration(4500);
+        mAnimationDrawable.start();
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+
+        mProgres = new ProgressDialog(getContext());
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkLogin();
+            }
+
+        });
+
+        return view;
+    }
+
+    /**
+     * Login via email, and get result if succeed or failed.
+     */
+    private void checkLogin() {
+        String email = mLoginEmailField.getText().toString().trim();
+        String password = mLoginPasswordField.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mProgres.setMessage("Checking Login");
+            mProgres.show();
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        mProgres.dismiss();
+                        checkUserExist();
+                    } else {
+                        mProgres.dismiss();
+                        Toast.makeText(getActivity(), "Failed to login", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Checking if user already exist in database.
+     * If not send user to SetupActivity.
+     */
+    private void checkUserExist() {
+        final String userId = mAuth.getCurrentUser().getUid();
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(userId)) {
+                    updateUI();
+                } else {
+                    goToSetupActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    /**
+     * Open MainActivity.
+     * Called when user is authenticated.
+     */
+    private void updateUI() {
+        //TODO: app crash here
+        Intent accountIntent = new Intent(getActivity(), MainActivity.class);
+        startActivity(accountIntent);
+        getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        getActivity().finish();
+    }
+
+    /**
+     * Open SetupActivity.
+     * Called when user is logged for first time and don't have set profile picture and name.
+     */
+    private void goToSetupActivity() {
+        Intent accountIntent = new Intent(getActivity(), MainActivity.class);
+        startActivity(accountIntent);
+        getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        getActivity().finish();
+    }
+
+}
