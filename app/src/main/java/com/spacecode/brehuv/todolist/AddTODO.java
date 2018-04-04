f32bfd7bd4310454de5b158597e7846330452539
Jakub Brehuv <kubek.breha@gmail.com>
2018-04-04 16:49:23 +0200

    add button with drawables

--- app/src/main/java/com/spacecode/brehuv/todolist/AddTODO.java
+++ app/src/main/java/com/spacecode/brehuv/todolist/AddTODO.java
@@ -1,13 +1,7 @@
 package com.spacecode.brehuv.todolist;
 
-import android.app.ActionBar;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class AddTODO extends AppCompatActivity {
 
